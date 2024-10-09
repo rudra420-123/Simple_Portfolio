6292476275fe2bfea1d9c609b1234191a8bcbfc7
Rudra Prasad <dipu13562@gmail.com>
2024-10-09 17:25:28 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import { Link } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const Navbar = () => {
+  return (
+    <AppBar position="static" sx={{ backgroundColor: '#1d1d1d' }}>
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" sx={{ flexGrow: 1, color: '#e0e0e0' }}>
+          Rudra's Portfolio
+        </Typography>
+        <Button sx={{ color: '#90caf9' }} component={Link} to="/">
+          Home
+        </Button>
+        <Button sx={{ color: '#90caf9' }} href="#about">
+          About
+        </Button>
+        <Button sx={{ color: '#90caf9' }} href="#skills">
+          Skills
+        </Button>
+        <Button sx={{ color: '#90caf9' }} href="#projects">
+          Projects
+        </Button>
+        <Button sx={{ color: '#90caf9' }} href="#contact">
+          Contact Me
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
